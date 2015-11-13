09a6d2a8a65b1082f120cddd8eb98262a65c18e6
perinikhil <peri.nikhil10@gmail.com>
2015-11-14 00:09:14 +0530

    added '/admin' route

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,13 @@
 <?php
 
-// Route::get('/', function(){
-//   phpinfo();
-// });
+Route::post('/admin', function(){
+  $password = Hash::make(Input::get('password'));
+  if(strcmp($password,'$2y$10$6pCJ0thMn3LbxzjjnQ78y.UinZyEfCrFE4Gapj5Uk4LEuExkMjCOS'))
+    return Response::json([
+      'success' => true,
+      'alert' => Messages::$loginSuccess
+    ]);
+});
 
 Route::resource('projects', 'ProjectController', ['except' => ['create', 'edit']]);
 Route::resource('projects.images', 'ImageController', ['except' => ['create', 'edit']]);

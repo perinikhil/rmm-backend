ef33c0243506aa52eb5065d86469f136649b95e0
perinikhil <peri.nikhil10@gmail.com>
2015-11-19 19:36:44 +0530

    added route-prefix api/

--- app/routes.php
+++ app/routes.php
@@ -1,13 +1,17 @@
 <?php
 
-Route::post('/admin', function(){
-  $password = Hash::make(Input::get('password'));
-  if(strcmp($password,'$2y$10$6pCJ0thMn3LbxzjjnQ78y.UinZyEfCrFE4Gapj5Uk4LEuExkMjCOS'))
-    return Response::json([
-      'success' => true,
-      'alert' => Messages::$loginSuccess
-    ]);
-});
+Route::group(array('prefix' => 'api'), function() {
 
-Route::resource('projects', 'ProjectController', ['except' => ['create', 'edit']]);
-Route::resource('projects.images', 'ImageController', ['except' => ['create', 'edit']]);
+  Route::post('/login', function(){
+    $password = Hash::make(Input::get('password'));
+    if(strcmp($password,'$2y$10$6pCJ0thMn3LbxzjjnQ78y.UinZyEfCrFE4Gapj5Uk4LEuExkMjCOS'))
+      return Response::json([
+        'success' => true,
+        'alert' => Messages::$loginSuccess
+      ]);
+  });
+
+  Route::resource('projects', 'ProjectController', ['except' => ['create', 'edit']]);
+  Route::resource('projects.images', 'ImageController', ['except' => ['create', 'edit']]);
+
+});
